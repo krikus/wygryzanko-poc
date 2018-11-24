b00aaa9b22de2f604778abfab98526aa60f7390b
Kamil <kamil@instapage.com>
2018-11-24 17:51:47 +0100

    Capped collection

--- backend/src/models/Content.js
+++ backend/src/models/Content.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   title: 'string',
   body: 'string'
+}, {
+  capped: 3,
 });
 
 const Content = mongoose.model('Content', schema);
